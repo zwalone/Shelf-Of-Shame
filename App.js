2e17218f8e7f110480fe479d552295647639d432
Dariusz Momot <dariusz.momot98@gmail.com>
2021-04-25 18:25:39 +0200

    Add firebase

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import firebaseConfig from './Api';
 
+
+firebase.initializeApp(firebaseConfig);
 export default function App() {
   return (
     <View style={styles.container}>
